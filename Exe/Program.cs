358a15a934c854d5b87529ef0795b0008afd7aab
Jeremy Banka <hello@jeremybanka.com>
2021-05-07 15:34:52 -0700

    🚧 add first question to my program

--- Exe/Program.cs
+++ Exe/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Bakery.Models;
 
 namespace Bakery.Executable
@@ -7,17 +8,36 @@ namespace Bakery.Executable
   {
     public static void Main()
     {
-      Console.WriteLine("Hello!");
-      string userInput = Console.ReadLine();
+      Sesh session = new();
+      Console.WriteLine("Hello! And welcome to Pierre's Bakery.");
+      Console.WriteLine("At this bakery, we make BREAD ($5) and PASTRY ($2).");
+      Console.WriteLine("Type 'OK' if you understand.");
+      Console.WriteLine("Type 'MORE' if you would like to know more.");
+      FirstQ();
+      int tutorialIdx = 0;
+    }
+    public static void FirstQ()
+    {
       try
       {
-        if (userInput == "Hello!") Console.WriteLine("Nice to see you!");
-        else throw new ArgumentException("Well that wasn't very nice.");
+        string userInput = Console.ReadLine();
+        switch (userInput)
+        {
+          case "OK":
+            Console.WriteLine("Great. So what do you want?");
+            break;
+          case "MORE":
+            Console.WriteLine("Buying 2 BREAD makes the third BREAD free!");
+            Console.WriteLine("And 3 PASTRY would normally be $6. But just for you, it's $5.");
+            break;
+          default: throw new();
+        }
+
       }
-      catch (ArgumentException e)
+      catch (ArgumentException)
       {
-        Console.WriteLine(e);
-        Main();
+        Console.WriteLine("You gotta work with me here, bud.");
+        FirstQ();
       }
     }
   }
