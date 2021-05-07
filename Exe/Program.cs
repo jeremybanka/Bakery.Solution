769916a88d16396054608481a9d9b854902ee014
Jeremy Banka <hello@jeremybanka.com>
2021-05-07 15:45:46 -0700

    ✨ add second question giving pricing and stat feedback on your order

--- Exe/Program.cs
+++ Exe/Program.cs
@@ -15,6 +15,8 @@ namespace Bakery.Executable
       Console.WriteLine("Type 'MORE' if you would like to know more.");
       FirstQ();
       int tutorialIdx = 0;
+      SecondQ(session, tutorialIdx, "");
+
     }
     public static void FirstQ()
     {
@@ -40,5 +42,91 @@ namespace Bakery.Executable
         FirstQ();
       }
     }
+    public static void SecondQ(Sesh session, int tutorialIdx, string lastInput)
+    {
+      var tutorialMessages = new string[]
+      {
+        "Type 'BREAD' for BREAD, or 'PASTRY' for PASTRY.",
+        "By the way, just type a number if you'd like multiple of something.",
+        "Also, you can repeat your last request with the enter key."
+      };
+      if (tutorialIdx >= 0 && tutorialIdx < tutorialMessages.Length)
+      {
+        Console.WriteLine(tutorialMessages[tutorialIdx]);
+      }
+      try
+      {
+        string userInput = Console.ReadLine();
+        if (userInput == "" & lastInput != "") userInput = lastInput;
+        lastInput = userInput;
+        switch (userInput)
+        {
+          case "OK":
+            Console.WriteLine("I said 'BREAD' or 'PASTRY'");
+            throw new();
+          case "bread":
+            Console.WriteLine("I'm hard of hearing, son, please speak up.");
+            throw new();
+          case "pastry":
+            Console.WriteLine("What was that? Did you say 'PAISLEY'?");
+            throw new();
+          case "PAISELY":
+            Console.WriteLine("We don't have PAISELY here.");
+            throw new();
+          case "BREAD":
+            session.Add(new Bread());
+            break;
+          case "PASTRY":
+            session.Add(new Pastry());
+            break;
+          default:
+            try
+            {
+              int number = int.Parse(userInput);
+              Console.WriteLine($"What do you want {number} of?");
+              userInput = Console.ReadLine();
+              if (number > 100)
+              {
+                Console.WriteLine($"That's a lot of {userInput}!");
+              }
+              switch (userInput)
+              {
+                case "BREAD":
+                  while (number > 0)
+                  {
+                    session.Add(new Bread());
+                    number--;
+                  }
+                  break;
+                case "PASTRY":
+                  while (number > 0)
+                  {
+                    session.Add(new Pastry());
+                    number--;
+                  }
+                  break;
+                default: throw new();
+              }
+            }
+            catch (Exception)
+            {
+              Console.WriteLine("But we don't have that here.");
+              throw;
+            }
+            break;
+        }
+      }
+      catch (Exception)
+      {
+        SecondQ(session, -1, "");
+      }
+      int breadCount = session.Order.Where(bg => bg.GetType().Name == "Bread").Count();
+      int pastryCount = session.Order.Where(bg => bg.GetType().Name == "Pastry").Count();
+      Console.WriteLine($"order: {breadCount} BREAD / {pastryCount} PASTRY.");
+      Console.WriteLine($"subtotal: ${session.Subtotal}");
+      tutorialIdx++;
+      SecondQ(session, tutorialIdx, lastInput);
+    }
+
   }
 }
